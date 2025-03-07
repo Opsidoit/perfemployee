11242d717813a36eb6bc9817cf2af957c9456f7e
Opsidoit <simone.obri@hotmail.it>
2025-03-07 11:30:08 +0000

    cv builder done

--- src/components/cv/CVPreviewModal.tsx
+++ src/components/cv/CVPreviewModal.tsx
@@ -175,7 +175,7 @@ const CVPreviewModal: React.FC<CVPreviewModalProps> = ({
             ) : (
               <>
                 <Save className="mr-2 h-4 w-4" />
-                {showTitleInput ? "Confirm Save" : "Save CV"}
+                {showTitleInput ? "Save" : "Save CV"}
               </>
             )}
           </Button>
